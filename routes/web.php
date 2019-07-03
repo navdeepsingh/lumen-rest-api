5a812f30bf9e898ef60fa8521362642410ae32f7
Navdeep Singh <navdeep.er@gmail.com>
2019-07-03 19:59:56 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('/public/phpinfo', function () {
+$router->get('/phpinfo', function () {
     return phpinfo();
 });
 
